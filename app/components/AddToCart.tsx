dcf1c1a0645404639b56fd4f10db7de5cf0d145c
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-02-06 10:22:25 +0545

    first commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("serever component")}>
+        Add To Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
---
+++ app/users/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+    // next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+  return (
+    <div>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserPage;
