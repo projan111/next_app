e3e8337546486a6e0fbb18a78f49e569c5eb022a
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-02-08 23:56:00 +0545

    background setups

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { BackgroundGradientAnimation } from "./components/ui/background-gradient-animation";
+import { CardContainer } from "./components/ui/3d-card";
+import { SparklesCore } from "./components/ui/sparkles";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+      <div className="relative">
+        <div className="card absolute z-10 flex items-center w-full h-full justify-center">
+          <h1 className="font-bold uppercase text-3xl px-5 py-36 rounded-full">
+            Welcome to webX
+          </h1>
+        </div>
+        <BackgroundGradientAnimation />
+      </div>
     </main>
   );
 }
